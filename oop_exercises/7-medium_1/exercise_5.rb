b4ce27b22019b78674ee6acb162c8d30eaff0ee3
Andy Burnett <38389218+ABurnett7@users.noreply.github.com>
2025-02-01 16:57:47 -0700

    Refactored Medium Exercise 5 some

--- oop_exercises/7-medium_1/exercise_5.rb
+++ oop_exercises/7-medium_1/exercise_5.rb
@@ -56,6 +56,10 @@ class Minilang
     end
   end
 
+  def pop
+    @stack.pop
+  end
+
   def is_integer?(string)
     string.to_i.to_s == string
   end
@@ -69,33 +73,67 @@ class Minilang
   end
 
   def command_add
-    self.register += @stack.pop 
+    self.register += pop 
   end
 
   def command_sub
-    self.register -= @stack.pop
+    self.register -= pop
   end
 
   def command_mult
-    self.register = register * @stack.pop
+    self.register = register * pop
   end
 
   def command_div
-    self.register = register / @stack.pop
+    self.register = register / pop
   end 
   
   def command_mod 
-    self.register = register % @stack.pop
+    self.register = register % pop
   end 
   
   def command_pop
-    self.register = @stack.pop
+    self.register = pop
   end
 
   def command_print
     puts register
   end
 end
+
+Minilang.new('PRINT').eval
+# 0
+
+Minilang.new('5 PUSH 3 MULT PRINT').eval
+# 15
+
+Minilang.new('5 PRINT PUSH 3 PRINT ADD PRINT').eval
+# 5
+# 3
+# 8
+
+Minilang.new('5 PUSH 10 PRINT POP PRINT').eval
+# 10
+# 5
+
+Minilang.new('5 PUSH POP POP PRINT').eval
+# Empty stack!
+
+Minilang.new('3 PUSH PUSH 7 DIV MULT PRINT ').eval
+# 6
+
+Minilang.new('4 PUSH PUSH 7 MOD MULT PRINT ').eval
+# 12
+
+Minilang.new('-3 PUSH 5 XSUB PRINT').eval
+# Invalid token: XSUB
+
+Minilang.new('-3 PUSH 5 SUB PRINT').eval
+# 8
+
+Minilang.new('6 PUSH').eval
+# (nothing printed; no PRINT commands)
+
 
 Minilang.new('PRINT').eval
 # 0
